d6b809a25980d781cd6b57e988358afbfdb0c5f5
Ashish Kumar <ashishkumar901336@gmail.com>
2025-04-22 00:47:09 +0530

    updated desc

--- App/drug_app.py
+++ App/drug_app.py
@@ -49,7 +49,7 @@ examples = [
 
 title = "Drug Classification"
 description = "Enter the details to correctly identify Drug type?"
-article = "This app is a part of the Beginner's Guide to CI/CD for Machine Learning. It teaches how to automate training, evaluation, and deployment of models to Hugging Face using GitHub Actions."
+article = "This app automates training, evaluation, and deployment of models to Hugging Face using GitHub Actions."
 
 
 gr.Interface(
